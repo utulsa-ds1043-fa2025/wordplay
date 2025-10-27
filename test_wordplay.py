2eac49cd9ce87493eaed140bb02f30cc2da8a7cf
Matthew Butler <matthew-butler@utulsa.edu>
2025-10-27 10:49:09 -0500

    Added test function stubs, and a complete test for wordplay.is_sorted

--- test_wordplay.py
+++ test_wordplay.py
@@ -1,9 +1,47 @@
 import wordplay
+import wordplay
+import random
+
+
+random.seed()
+
 
 def test_merge():
-    a = list(range(0, 10, 2)) # Even numbers 0..8, 5 total
-    b = list(range(1, 10, 2)) # Odd numbers 1..9, 5 total
-    merged = wordplay.merge(a, b)
+    a: list[int] = list(range(0, 10, 2)) # Even numbers 0..8, 5 total
+    b: list[int] = list(range(1, 10, 2)) # Odd numbers 1..9, 5 total
+    merged: list[int] = wordplay.merge(a, b)
     assert merged == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert len(a) == 5 and len(b) == 5
     assert len(merged) == 10
+
+def test_nested_sum():
+    assert False
+
+def test_cumulative_sum():
+    assert False
+
+
+def test_middle():
+    assert False
+
+def test_chop():
+    assert False
+
+
+def test_is_sorted():
+    a: list[int] = list(range(10))
+    b: list[int] = a[:]
+    assert wordplay.is_sorted(a)
+    for _ in range(10):
+        random.shuffle(b)
+        if a == b:
+            assert wordplay.is_sorted(b)
+        else:
+            assert not wordplay.is_sorted(b)
+
+def test_reverse_pairs():
+    assert False
+
+
+def test_interlocking_pairs():
+    assert False
