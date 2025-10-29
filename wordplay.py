fef5652af77706968e90ec02462da8ae14041e4f
Matthew Butler <matthew-butler@utulsa.edu>
2025-10-29 10:49:54 -0500

    Added and cleaned up comments, added a comparison function for reverse_pairs using lists and sets

--- wordplay.py
+++ wordplay.py
@@ -1,5 +1,7 @@
 import timeit
+import sys
 from collections.abc import Collection
+
 
 def appended_words(filename: str) -> list[str]:
     words: list[str] = []
@@ -36,7 +38,7 @@ def compare_added_appended(filename: str) -> None:
 def merge(a: list, b: list) -> list:
     "Non-destructively merges two sorted lists"
     merged: list = []
-    positions: list[int] = [0, 0] # Keeps track of current position in a and b
+    positions: list[int] = [0, 0]  # Keeps track of current position in a and b
     # Walk through a and b, adding the smallest element at the current position
     # for each list
     while positions[0] < len(a) and positions[1] < len(b):
@@ -98,30 +100,35 @@ def cumulative_sum(numbers: list[int]) -> list[int]:
 
 def middle(seq: list) -> list:
     "Creates a new list that contains all but the first and last elements of seq"
-    return seq[1:-1]
+    return seq[1:-1]  # the [] operator with a slice creates a new object
 
 
 def chop(seq: list) -> None:
     "Removes the first and last elements of seq"
-    seq.pop(0)
-    seq.pop(-1)
+    seq.pop(0)   # seq refers to a list in memory
+    seq.pop(-1)  # calling seq.pop modifies that list
     return
 
 
 def is_sorted(seq: list) -> bool:
     "Tests whether the sequence is sorted in ascending order"
-    # compare element 0 with element 1, element 1 with element 2, etc (n-1 comparisons)
-    for index, element in enumerate(seq[:-1]): # Remember guardrails!
+    # Compare element 0 with element 1, element 1 with element 2, etc (n-1 comparisons)
+    # Since we are comparing the current element with the next element,
+    # only iterate through the first n-1 elements to avoid an IndexError
+    for index, element in enumerate(seq[:-1]):
         if element > seq[index + 1]:
             return False
     return True
 
-def reverse_pairs(words: list[str]) -> list[tuple[str]]:
+
+def reverse_pairs(words: Collection[str]) -> list[tuple[str]]:
     "Find all pairs of words which are reverse-related"
-    pairs = []
-    for word in words: # we do this block n times
+    pairs: list[tuple[str]] = []
+    for word in words:  # we do this block n times
         # We can search for an item in a collection using `in`
-        if word[::-1] in words: # Usually this operator goes through n words
+        # If words is a list, `in` takes ~n operations
+        # If words is a set, `in` takes ~1 operation
+        if word[::-1] in words:
             pairs.append((word, word[::-1]))
     return pairs
 
@@ -130,7 +137,7 @@ def reverse_pairs_list(words: list[str]) -> list[tuple[str]]:
     "Find all pairs of words which are reverse-related"
     pairs = []
     ops = 0
-    for index, word in enumerate(words): # we do this block n times
+    for index, word in enumerate(words):  # we do this block n times
         # This block mimics the in/contains operator #
         for candidate in words:
             ops += 1
@@ -141,14 +148,36 @@ def reverse_pairs_list(words: list[str]) -> list[tuple[str]]:
     print(f'Total Comparisons = {ops}')
     return pairs
 
+
 def reverse_pairs_set(words: set[str]) -> list[tuple[str]]:
     "Find all pairs of words which are reverse-related"
     ops = 0
     pairs = []
-    for word in words: # we do this block n times
+    for word in words:  # we do this block n times
         ops += 1
         # We can search for an item in a collection using `in`
-        if word[::-1] in words: # This is one operation
+        if word[::-1] in words:  # This is one operation
             pairs.append((word, word[::-1]))
     print(f'Total Comparisons = {ops}')
     return pairs
+
+
+def compare_list_set_search(filename: str) -> None:
+    """Uses timeit to compare the performance of searching lists and sets using the `in` operator"""
+    word_list: list[str] = []
+    word_set: set[str] = set()
+    with open(filename, 'r', encoding='utf-8') as wordfile:
+        for word in (line.strip() for line in wordfile):
+            word_list.append(word)
+            word_set.add(word)
+    print('WARNING: This may take a few minutes!')
+    list_time = timeit.timeit(f"reverse_pairs({word_list})",
+                              globals=globals(),
+                              number=2)
+    set_time = timeit.timeit(f"reverse_pairs({word_set})",
+                             globals=globals(),
+                             number=2)
+    print("| data structure | elements | memory used (B) | search time (s) |")
+    print("|----------------|----------|-----------------|-----------------|")
+    print(f'| list           | {len(word_list):>8} | {sys.getsizeof(word_list):>15} | {list_time:>15} |')
+    print(f'| set            | {len(word_set):>8} | {sys.getsizeof(word_set):>15} | {set_time:>15} |')
